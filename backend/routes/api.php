cc9ff658219b969efafc47762cec35eb755f1c57
LucasELopes <lucaslopeslll412@gmail.com>
2025-11-12 09:43:54 -0300

    feat: add new roles to user

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,17 +2,21 @@
 
 use App\Http\Controllers\Auth\AuthenticatedTokenController;
 use App\Http\Controllers\SubscriptionController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthenticatedTokenController::class, 'store']);
-Route::post('/logout', [AuthenticatedTokenController::class, 'destroy'])->middleware('auth:sanctum');
-
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
-});
 
 Route::middleware(['auth:sanctum'])->group(function() {
+    Route::post('/logout', [AuthenticatedTokenController::class, 'destroy'])->middleware('auth:sanctum');
+
     Route::apiResource('/subscriptions', SubscriptionController::class);
+
+    // Rota do administrador
+    Route::middleware(['can:admin'])->group(function () {
+        Route::apiResource('/users', UserController::class);
+    });
 });
+
 
