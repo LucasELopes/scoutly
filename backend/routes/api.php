164111ddd8fbe9cb4be9711ea2126c302cf0fc3d
LucasELopes <lucaslopeslll412@gmail.com>
2025-11-13 16:03:16 -0300

    refactor: routes api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,8 +4,6 @@ use App\Http\Controllers\Auth\AuthenticatedTokenController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\SubscriptionController;
 use App\Http\Controllers\UserController;
-use App\Models\Product;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthenticatedTokenController::class, 'store']);
@@ -13,12 +11,15 @@ Route::post('/login', [AuthenticatedTokenController::class, 'store']);
 Route::middleware(['auth:sanctum'])->group(function() {
     Route::post('/logout', [AuthenticatedTokenController::class, 'destroy'])->middleware('auth:sanctum');
 
-    Route::apiResource('/products', ProductController::class);
-    Route::apiResource('/subscriptions', SubscriptionController::class);
+    Route::apiResource('/products', ProductController::class)->except('index');
+    Route::apiResource('/subscriptions', SubscriptionController::class)->except('index');
 
     // Rota do administrador
     Route::middleware(['can:admin'])->group(function () {
         Route::apiResource('/users', UserController::class);
+        
+        Route::get('subscriptions', [SubscriptionController::class, 'index']);
+        Route::get('products', [ProductController::class, 'index']);
     });
 });
 
