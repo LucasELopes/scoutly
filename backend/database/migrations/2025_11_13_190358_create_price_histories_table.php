a8395fec08f470aa36c22d0ef7e6ed7aa41fd0ea
LucasELopes <lucaslopeslll412@gmail.com>
2025-11-14 13:52:15 -0300

    feat: add onCascade in price_histories migration

--- backend/database/migrations/2025_11_13_190358_create_price_histories_table.php
+++ backend/database/migrations/2025_11_13_190358_create_price_histories_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('price_histories', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->foreignUuid('product_id')->references('id')->on('products');
+            $table->foreignUuid('product_id')->references('id')->on('products')->cascadeOnDelete()->cascadeOnUpdate();
             $table->decimal('price', 10, 2);
             $table->timestamp('checked_at');
         });
